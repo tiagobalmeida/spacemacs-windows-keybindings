bdd478b58dc56db7c234a77ca23bca40a34fd4a1
Tiago Almeida <jumpifzero@gmail.com>
2019-05-20 11:04:14 +0100

    adds C-f4, M-f4, C-o support

--- keybindings.el
+++ keybindings.el
@@ -14,31 +14,44 @@
 ;; C-z is undo. C-RET starts rectangle selection mode.
 (cua-mode t)
 
+;; TODO
+;; How do you get the function behind a key? Is it (lookup-key global-map (kbd "C-h"))
+;; or (key-binding (kbd "C-x C-f"))
+
 ;; ******************* Function keys ********************
 ;; Move help to F1. Must happen before we rebind C-h.
 ;(global-set-key [f1] (lookup-key global-map (kbd "C-h")))
 (bind-key* "<f2>"    #'er/expand-region)      ;; From MS Excel muscle memory.
 (bind-key* "C-<f4>"  #'kill-this-buffer)
+(when windows-keybindings-a-f4-closes
+  (bind-key* "M-<f4>" (key-binding (kbd "C-x C-c"))))
+(when windows-keybindings-c-tab-other-window
+  (bind-key* "C-TAB" (key-binding (kbd "C-x o")))
+  (bind-key* "C-<tab>" (key-binding (kbd "C-x o"))))
 
 ;; ******************* Marking the buffer **************
 (bind-key* "C-S-a"   #'mark-whole-buffer)
 
 ;; Dragging lines. TODO: Needs to drag regions as well.
-(bind-key* "M-<up>" #'drag-stuff-up)
-(bind-key* "M-<down>" #'drag-stuff-down)
+;; (bind-key* "M-<up>" #'drag-stuff-up)
+;; (bind-key* "M-<down>" #'drag-stuff-down)
 
 ;; This moves isearch-forward from C-s to C-f. We also need to adjust the keys within the isearch-mode-map.
 (when windows-keybindings-c-f-searches
   (bind-key "C-f"      #'isearch-forward)
   (bind-key "C-f"      #'isearch-repeat-forward isearch-mode-map))
 
-(bind-key* "C-o"     #'spacemacs/helm-find-files)
-(bind-key* "C-n"     #'spacemacs/new-empty-buffer)
+(when windows-keybindings-c-o-opens
+  (bind-key* "C-o" (key-binding (kbd "C-x C-f"))))
+
+(when windows-keybindings-c-n-creates
+  (bind-key* "C-n" #'spacemacs/new-empty-buffer))
 
 (when windows-keybindings-c-s-saves
   (unbind-key "C-s"    isearch-mode-map)
-  (bind-key* "C-s"     #'save-buffer)
-  (bind-key* "C-p"     #'helm-projectile-find-file))
+  (bind-key* "C-s"     #'save-buffer))
+
+;;(bind-key* "C-p"     #'helm-projectile-find-file)
 
 ;; Increasing / decreasing font size with C+Scroll
 ;; Don't understand why it doesn't work by default..
